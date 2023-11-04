b68f7f1ff5dc41c6fba5929572533ed1a3de205c
Franki Callard <francescalily@outlook.com>
2023-11-04 19:40:02 +0000

    public

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  root: "./client",
+  root: "./public",
   build: {
     rollupOptions: {
       // This is where you can input the multiple files
       input: {
-        main: "./client/index.html",
+        main: "./public/index.html",
         // Add other pages with relative paths from root here
-        page1: "./client/Generative/index.html",
-        page2: "./client/Websites/index.html",
-        page3: "./client/Contact/index.html",
+        page1: "./public/Generative/index.html",
+        page2: "./public/Websites/index.html",
+        page3: "./public/Contact/index.html",
         // Continue for as many HTML files as you have
       },
     },
