d6484325058c217d596e6c645fafcedac62aecf9
Franki Callard <francescalily@outlook.com>
2023-11-04 19:43:17 +0000

    pushing

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,16 @@ export default defineConfig({
       input: {
         main: "./public/index.html",
         // Add other pages with relative paths from root here
+
+        page2: "./public/sketch.js",
         page1: "./public/Generative/index.html",
+        page1: "./public/Generative/index.js",
+        page1: "./public/Generative/styles.css",
         page2: "./public/Websites/index.html",
+        page2: "./public/Websites/styles.css",
         page3: "./public/Contact/index.html",
+        page4: "./public/Contact/script.js",
+        page5: "./public/Contact/style.css",
         // Continue for as many HTML files as you have
       },
     },
